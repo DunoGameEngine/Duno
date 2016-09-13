98ef1c5950835021e1fea3fb0ad3ed1d9c2a3a6b
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2016-09-13 20:28:46 +0100

    added basic display class

--- Duno/Duno-Core/Display/Display.h
+++ Duno/Duno-Core/Display/Display.h
@@ -1,1 +1,32 @@
 #pragma once
+namespace Duno {namespace Graphics {
+
+		class Display {
+
+		private:
+			//const long m_ScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+			//const long m_ScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+			const char* m_title;
+			int m_width;
+			int m_height;
+			int m_posX;
+			int m_posY;
+			int m_fps;
+			bool m_useVsync;
+
+			//GLFWwindow* window = NULL;
+
+		public:
+			bool running = true;
+
+			Display();
+			~Display();
+
+			void createDisplay(char* title = "Nein_Engine", int width = 720, int height = 640,
+				int posX = -1, int posY = -1, int fps = 60, bool useVsync = false);
+			void updateDisplay();
+			void closeDisplay();
+
+		};
+	}}
