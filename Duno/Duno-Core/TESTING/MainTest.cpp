a07606186d31e57f165c8e1d9db0ae51e04426db
BenJilks <theZombiebrine2@gmail.com>
2016-09-14 17:47:37 +0100

    MainTest Logs

--- Duno/Duno-Core/TESTING/MainTest.cpp
+++ Duno/Duno-Core/TESTING/MainTest.cpp
@@ -1,18 +1,27 @@
 #include "../Graphics/Display/Display.h"
+#include "../GameUtil/Logger.h"
 
 int main() {
+
+	Logger::init();
+	Logger::setSpace("GameLoader");
 
 	using namespace Duno;
 	using namespace Graphics;
 
+	Logger::logln("Loading Display");
 	Display newDisplay;
 	newDisplay.createDisplay("swag");
 
+	Logger::logln("Starting Main Game Loop");
+	Logger::setSpace("GameLoop");
 	while (newDisplay.running) {
 
 		newDisplay.updateDisplay();
 
 	}
+	Logger::back();
+	Logger::logln("Game Closing");
 	newDisplay.closeDisplay();
 
 	return 0;
