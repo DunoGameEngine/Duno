83f204c46f6a421461d427475ee8ed1869298995
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2016-09-14 19:28:14 +0100

    Added the loader class

---
+++ Duno/Duno-Core/Graphics/renderEngine/Loader.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+#include <list>
+#include <vector>
+namespace Duno{namespace Graphics{namespace RenderEngine{
+
+	class Loader {
+
+	private:
+
+		std::list<int> vbos;
+		std::list<int> vaos;
+
+	public:
+
+
+
+	};
+
+
+} } }
