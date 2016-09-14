36eeff866e38e3e837525860da876fc8176e21b3
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2016-09-14 22:36:20 +0100

    PlainModel class completed

--- Duno/Duno-Core/Graphics/renderEngine/Types/PlainModel.cpp
+++ Duno/Duno-Core/Graphics/renderEngine/Types/PlainModel.cpp
@@ -3,3 +3,28 @@
 Duno::Graphics::RenderEngine::PlainModel::PlainModel(int vaoID, int vertex, int vaoLength, bool index)
 {
 }
+
+int Duno::Graphics::RenderEngine::PlainModel::getVaoID()
+{
+	return vaoID;
+}
+
+int Duno::Graphics::RenderEngine::PlainModel::getVaoLength()
+{
+	return vaoLength;
+}
+
+void Duno::Graphics::RenderEngine::PlainModel::setVaoLength(int length)
+{
+	vaoLength = length;
+}
+
+int Duno::Graphics::RenderEngine::PlainModel::getVertexNumber()
+{
+	return vertexNumber;
+}
+
+bool Duno::Graphics::RenderEngine::PlainModel::getHasIndexBuffer()
+{
+	return hasIndexBuffer;
+}
--- Duno/Duno-Core/Graphics/renderEngine/Types/PlainModel.h
+++ Duno/Duno-Core/Graphics/renderEngine/Types/PlainModel.h
@@ -5,7 +5,10 @@ namespace Duno{namespace Graphics{ namespace RenderEngine{
 	class PlainModel {
 
 	private:
-
+		int vaoID;
+		int vaoLength;
+		int vertexNumber;
+		bool hasIndexBuffer;
 	public:
 
 		//a plain vertex model has to contain
@@ -13,6 +16,21 @@ namespace Duno{namespace Graphics{ namespace RenderEngine{
 		//length of the vao and whever it uses indices buffer
 		PlainModel(int vaoID, int vertex, int vaoLength, bool index);
 
+		//get an ID of model's VAO
+		int getVaoID();
+
+		//get the size of the VAO
+		int getVaoLength();
+
+		//set the size of the VAO
+		//used in the renderer
+		void setVaoLength(int length);
+
+		//get the number of vertices
+		int getVertexNumber();
+
+		//if the model has an indices buffer
+		bool getHasIndexBuffer();
 
 	};
 
