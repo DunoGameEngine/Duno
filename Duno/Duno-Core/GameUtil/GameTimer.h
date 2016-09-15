53fd9cf2671da37c42e09e0456d5d7c66ec1287e
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2016-09-15 09:46:21 +0100

    Update GameTimer.h
    nothing much, code is alright just change some comments

--- Duno/Duno-Core/GameUtil/GameTimer.h
+++ Duno/Duno-Core/GameUtil/GameTimer.h
@@ -6,13 +6,16 @@ class GameTimer
 public:
 	/* Returns the time in milloseconds */
 	static long currentTimeInMills();
-	/* Returns how long the last frame took to render and or update */
+	
+	/* Returns how long the last frame took to render and/or update */
 	float getFrameTimeSeconds() { return delta; }
-	/* Updates the times (called after every frame) */
+	
+	/* Updates the time (called after every frame) */
 	void update();
 private:
-	/* Keeps the time that the last frame started */
+	/* Keeps the time when the last frame started */
 	long lastFrameTime;
-	/* Keeps how long the last frame took to render and or update */
+	
+	/* Keeps how long the last frame took to render and/or update */
 	float delta;
 };
