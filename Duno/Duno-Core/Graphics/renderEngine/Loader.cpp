2588d99b8aa12ea3da7a093172055fb9b81739e6
EdwinRybarczyk <edwinrybarczyk4d@gmail.com>
2016-09-16 21:44:07 +0100

    Finished the Loader class

--- Duno/Duno-Core/Graphics/renderEngine/Loader.cpp
+++ Duno/Duno-Core/Graphics/renderEngine/Loader.cpp
@@ -2,16 +2,29 @@
 
 Duno::Graphics::RenderEngine::Loader::Loader()
 {
+	vaoLength = 0;
 }
 
 Duno::Graphics::RenderEngine::Loader::~Loader()
 {
+	cleanUp();
 }
 
 Duno::Graphics::RenderEngine::Types::PlainModel
 Duno::Graphics::RenderEngine::Loader::load(float data[], int indices[])
 {
+	//create new VAO
+	int VAO = createVAO();
 
+	if (!indices == NULL) {
+		bindIndicesBuffer(indices);
+	}
+
+	if (data != NULL) {
+		storeDataInVBO(3, data);
+	}
+
+	unbindVAO();
 
 	return Types::PlainModel(0,0,0,false); //have it blank for now
 }
@@ -19,6 +32,75 @@ Duno::Graphics::RenderEngine::Loader::load(float data[], int indices[])
 
 int Duno::Graphics::RenderEngine::Loader::createVAO()
 {
-	return 0;
+	GLuint vaoID;
+
+	//Generate new vao
+	glGenVertexArrays(1,&vaoID);
+
+	//add VAO to the list
+	vaos.push_back(vaoID);	
+
+	//bind the vao
+	glBindVertexArray(vaoID);	
+
+	return vaoID;
+}
+
+void Duno::Graphics::RenderEngine::Loader::unbindVAO()
+{
+	glBindVertexArray(0);
+}
+
+
+GLuint Duno::Graphics::RenderEngine::Loader::storeDataInVBO(int dataSize, float data[])
+{
+	//create new gl int variable
+	GLuint vboID;
+
+
+	//assign it to a new buffer
+	glGenBuffers(1, &vboID);
+
+	//add vboID to vbos list
+	vbos.push_back(vboID);
+
+	//bind the vbo
+	glBindBuffer(GL_ARRAY_BUFFER,vboID);
+	glVertexAttribPointer(vaoLength, dataSize, GL_FLOAT, false, 0, 0);
+
+	//increase the number of vbos in the vao
+	vaoLength++;
+
+	//unbind the vbo
+	glBindBuffer(GL_ARRAY_BUFFER,0);
+
+	return vboID;
+
+}
+
+
+void Duno::Graphics::RenderEngine::Loader::bindIndicesBuffer(int indices[])
+{
+
+	GLuint vboID;
+
+	glGenBuffers(GL_ELEMENT_ARRAY_BUFFER,&vboID);
+
+	vbos.push_back(vboID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof indices, &indices,GL_STATIC_DRAW);
+
+
+}
+
+void Duno::Graphics::RenderEngine::Loader::cleanUp()
+{
+	for (GLuint vbo : vbos) {
+		glDeleteBuffers(1,&vbo);																//delete vbos
+	}
+
+	for (GLuint vao : vaos) {
+		glDeleteVertexArrays(1,&vao);															//delete vaos
+	}
 }
 
--- Duno/Duno-Core/Graphics/renderEngine/Loader.h
+++ Duno/Duno-Core/Graphics/renderEngine/Loader.h
@@ -3,6 +3,7 @@
 #include <list>
 #include <vector>
 #include "Types\PlainModel.h"
+#include "../OpenGL/GL.h"
 namespace Duno{namespace Graphics{namespace RenderEngine{
 
 	class Loader {
@@ -11,20 +12,31 @@ namespace Duno{namespace Graphics{namespace RenderEngine{
 
 		//store VBOs and VAOs for memory management
 		//the list will be deleted at the end
-		std::list<int> vbos;
-		std::list<int> vaos;
+		std::list<GLuint> vbos;
+		std::list<GLuint> vaos;
 
 		//store the number of VBOs inside VAO
 		int vaoLength;
 
-
+		//VAO management (vao's are required in teh new OpenGL)
 		int createVAO();
+		void unbindVAO();
+
+		//VBO management + store in VAO
+		GLuint storeDataInVBO(int dataSize, float data[]);
+
+		//bind Indices and include them ni the VAO
+		void bindIndicesBuffer(int indices[]);
+
+		//Delete all the VBOs and VAOs
+		void cleanUp();
 	public:
 
 		//constructor and destructor of the class constructor
 		Loader();
 		~Loader();
 
+		//load data into the model, return new model
 		Types::PlainModel load(float data[], int indices[]);
 
 
